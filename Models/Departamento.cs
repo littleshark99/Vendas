fffb7a1835d1e6cc3808b894eeac6d7b5ec76bcc
littleshark99 <rafael.clare@hotmail.com>
2025-01-06 21:03:33 -0300

    Crianção de departamento

---
+++ Models/Departamento.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SalesWebMvc.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
